3faaa1d4f2abeb56625cb0b120c0f7242a21dae1
Sixin <ens>
2020-09-29 17:47:29 +0200

    test on cpu for reproducibility

--- code_rec2d/env.sh
+++ code_rec2d/env.sh
@@ -1,5 +1,4 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.2/lib64
-export KYMATIO_BACKEND=skcuda
-
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.2/lib64
+#export KYMATIO_BACKEND=skcuda
 export PATH="$PATH:~/anaconda3/bin"
-source activate phaseharmonics
+source activate phaseharmonics100
